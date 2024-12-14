9f8099c8046a924d7c5cd7f90d72fb4083d43a28
lewis031221 <1665054043@qq.com>
2024-12-14 17:29:32 +0800

    StudentVO修改

--- mybaits_demo/src/main/java/org/example/repository/entry/StudentVO.java
+++ mybaits_demo/src/main/java/org/example/repository/entry/StudentVO.java
@@ -1,4 +1,34 @@
 package org.example.repository.entry;
 
 public class StudentVO {
+
+    private int id;
+    private String name;
+    private String gender;
+    private int grade;
+    private String dept;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return "StudentVO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", grade=" + grade +
+                ", dept='" + dept + '\'' +
+                '}';
+    }
+
+
 }
